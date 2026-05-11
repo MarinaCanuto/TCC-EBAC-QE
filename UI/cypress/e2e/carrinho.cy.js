7b08611bc0ddfea130505dfe723a09a1158f9d47
Marina Canuto <maricanuto01.pt@mail.com>
2026-05-11 10:05:08 -0300

    test: adiciona cenarios de validacao para carrinho

--- UI/cypress/e2e/carrinho.cy.js
+++ UI/cypress/e2e/carrinho.cy.js
@@ -6,4 +6,49 @@ describe('Carrinho EBAC Shop', () => {
     cy.get('.woocommerce-message')
       .should('contain', 'foi adicionado no seu carrinho')
   })
+
+  it('Deve exibir o produto adicionado no carrinho', () => {
+    cy.adicionarProdutoAoCarrinho()
+
+    cy.get('.woocommerce-message a')
+      .click()
+
+    cy.get('.cart_item')
+      .should('be.visible')
+  })
+
+  it('Deve atualizar a quantidade do produto no carrinho', () => {
+    cy.adicionarProdutoAoCarrinho()
+
+    cy.get('.woocommerce-message a')
+      .click()
+
+    cy.get('.qty')
+      .clear()
+      .type('2')
+
+    cy.get('[name="update_cart"]')
+      .click()
+
+    cy.get('.woocommerce-message')
+      .should('be.visible')
+  })
+
+  it('Deve validar o limite máximo de 10 itens do mesmo produto', () => {
+    cy.adicionarProdutoAoCarrinho()
+
+    cy.get('.woocommerce-message a')
+      .click()
+
+    cy.get('.qty')
+      .clear()
+      .type('11')
+
+    cy.get('[name="update_cart"]')
+      .click()
+      cy.wait(1000)
+
+    cy.get('.woocommerce-error, .woocommerce-message')
+      .should('be.visible')
+  })
 })
