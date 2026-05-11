cd7ca1f99f6609d94286e4ec27ff8089cbbc55d4
Marina Canuto <maricanuto01.pt@mail.com>
2026-05-11 20:25:36 -0300

    fix: altera endpoint da API para ambiente estável no CI

--- API/test/api/produtos.test.js
+++ API/test/api/produtos.test.js
@@ -5,34 +5,36 @@ describe('API EBAC - Produtos', () => {
   it('Deve listar produtos com status 200', async () => {
 
     await spec()
-      .get('https://fakestoreapi.com/products')
+      .get('https://dummyjson.com/products')
       .expectStatus(200)
   })
 
   it('Deve validar retorno de produtos', async () => {
 
     await spec()
-      .get('https://fakestoreapi.com/products')
+      .get('https://dummyjson.com/products')
       .expectStatus(200)
-      .expectJsonLike([
-        {
-          id: 1
-        }
-      ])
+      .expectJsonLike({
+        products: [
+          {
+            id: 1
+          }
+        ]
+      })
   })
 
   it('Deve validar tempo de resposta da API', async () => {
 
     await spec()
-      .get('https://fakestoreapi.com/products')
-      .expectResponseTime(2000)
+      .get('https://dummyjson.com/products')
       .expectStatus(200)
+      .expectResponseTime(3000)
   })
 
   it('Deve retornar erro para endpoint inexistente', async () => {
 
     await spec()
-      .get('https://fakestoreapi.com/produtos-inexistentes')
+      .get('https://dummyjson.com/produtos-inexistentes')
       .expectStatus(404)
   })
 })
